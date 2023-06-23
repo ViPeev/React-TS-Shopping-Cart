66ee8b193a5c7b9a086aa32dc21af35945b2ff69
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-06-24 01:33:46 +0300

    Remove buttons added for each product in the cart

--- src/components/ShoppingCartItem.tsx
+++ src/components/ShoppingCartItem.tsx
@@ -1,6 +1,7 @@
 import { useCart } from "../context/CartContext";
-import { Stack } from "react-bootstrap";
+import { Stack, Button } from "react-bootstrap";
 import items from "../data/items.json";
+import { formatCurrency } from "../utils/formatCurrency";
 
 type CartItemProps = {
   id: number;
@@ -17,11 +18,28 @@ export function CartItem({ id, quantity }: CartItemProps) {
   }
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && <span className="text-muted fs-5">x{quantity}</span>}
+        </div>
+        <div className="text-muted fs-5">{formatCurrency(item.price)}</div>
+      </div>
+      <div className="text-muted fs-5">
+        {formatCurrency(item.price * quantity)}
+      </div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
     </Stack>
   );
 }
