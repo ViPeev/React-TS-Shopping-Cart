28960ce564f12686c8a181daa9781076003e6351
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-06-23 21:34:27 +0300

    initial commit, set routing and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { Navbar as NavbarBs, Nav, Container, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto ">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <circle cx="9" cy="21" r="1"></circle>
+            <circle cx="20" cy="21" r="1"></circle>
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+          </svg>
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.2rem",
+              height: "1.2rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(10%,30%)",
+            }}
+          ></div>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+}
