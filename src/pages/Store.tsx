82c1ea49574b6e65c289bd68efa81ac2970ae401
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-06-23 22:43:43 +0300

    added item data and components to store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,21 @@
+import { Item } from "../components/Item";
+import items from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+
 export function Store() {
-    return <h1>Store</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {items.map((item) => {
+          return (
+            <Col key={item.id}>
+              <Item {...item} />
+            </Col>
+          );
+        })}
+        <Col></Col>
+      </Row>
+    </>
+  );
 }
